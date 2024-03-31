57aad0578399e4bd8b509d17fc174df73c8aaef2
junaidjesan2 <junaidjesan2@gmail.com>
2024-03-31 17:05:07 +0600

    form added into signup and signin page

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSytyR2dgNCuXL4JLvOBnI9XlRWCwC5aE",
+  authDomain: "user-authentication-bc0f4.firebaseapp.com",
+  projectId: "user-authentication-bc0f4",
+  storageBucket: "user-authentication-bc0f4.appspot.com",
+  messagingSenderId: "1012988308060",
+  appId: "1:1012988308060:web:7e31acfffaaf7207e81499",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,40 @@
 import { Link } from "react-router-dom";
 
 export default function Login() {
+  const signUpFunction=(e)=>{
+    console.log(e)
+  }
   return (
-    <div className="bg-slate-500 flex flex-col space-y-7 w-3/6 mx-auto px-3 py-16 items-center rounded-lg mt-20">
-      <h1 className="font-semibold text-white">Login Now</h1>
-      <form action="" className=" flex flex-col space-y-5">
-        <input type="text" placeholder="name" className="px-2" />
-        <input type="text" placeholder="email" className="px-2" />
-        <input type="text" placeholder="password" className="px-2" />
-        <button className="py-2 px-3 rounded-xl bg-slate-700 text-white">
-          Submit
-        </button>
-      </form>
-      <div>
-        <Link to="/register">Register </Link>
+    <div className="min-h-screen lg:mt-28">
+      <div className="flex flex-col justify-center items-center space-y-5 bg-stone-400 py-28 rounded-xl">
+        <h1 className="text-3xl font-semibold">Sign IN</h1>
+        <form action="" className="flex flex-col space-y-5 w-3/6">
+          <div className="flex flex-col-reverse">
+            <input
+              className="py-2 px-2 rounded-lg"
+              type="text"
+              id="email"
+              placeholder="email"
+            />
+            <label htmlFor="email">Email</label>
+          </div>
+          <div className="flex flex-col-reverse">
+            <input
+              className="py-2 px-2 rounded-lg"
+              type="text"
+              id="password"
+              placeholder="Password"
+            />
+            <label htmlFor="password">Password</label>
+          </div>
+          <button
+            onSubmit={() => signUpFunction()}
+            className="py-2 rounded-lg bg-neutral-600"
+          >
+            Sign In
+          </button>
+        </form>
+        <Link to="/register">Register</Link>
       </div>
     </div>
   );
